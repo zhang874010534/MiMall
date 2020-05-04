f3635a5ab7cd85601441ea366627cb3424f0e3a6
zhang874010534 <z874010534@vip.qq.com>
2020-05-04 09:16:16 +0800

    轮播图分页和导航

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,37 @@
 <template>
-  <div>
-    index
-  </div>
+  <div class="container index">
+    <div class="swiper-box">
+      <swiper ref="mySwiper" :options="swiperOptions">
+        <swiper-slide>Slide 1</swiper-slide>
+        <swiper-slide>Slide 2</swiper-slide>
+        <swiper-slide>Slide 3</swiper-slide>
+        <swiper-slide>Slide 4</swiper-slide>
+        <swiper-slide>Slide 5</swiper-slide>
+        <div class="swiper-pagination" slot="pagination"></div>
+      </swiper>
+    </div>
 </template>
+<script>
+import { Swiper, SwiperSlide, directive } from 'vue-awesome-swiper';
+import 'swiper/css/swiper.css';
+export default {
+  name: 'index',
+  components: {
+    Swiper,
+    SwiperSlide
+  },
+  directives: {
+    swiper: directive
+  },
+  data() {
+    return {
+       swiperOptions: {
+          pagination: {
+            el: '.swiper-pagination'
+          },
+          // Some Swiper option/callback...
+        }
+    };
+  }
+};
+</script>
